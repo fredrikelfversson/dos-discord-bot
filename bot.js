8a63693121fd700d6bd065756ec8cb3868cafdbd
fredrikelfversson <38050776+fredrikelfversson@users.noreply.github.com>
2019-05-22 23:06:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -17,8 +17,10 @@ async function countEmojis(guild, channel, mega, uid) {
         if (lastID) {
             options.before = lastID;
         }
+        
         const messages = await channel.fetchMessages(options);
         msgArr = messages.array()
+        
         for (message in msgArr) {
             if(mega == false && msgArr[message].id == uid) {
                 messageFound = true;
@@ -30,12 +32,13 @@ async function countEmojis(guild, channel, mega, uid) {
                 } 
             }
         }
+        
         lastID = messages.last().id;
+        
         if (messages.size != 100 || messageFound) {
             break;
         }
     }
-    console.log(emojiDict)
     return await {
         emojiDict:emojiDict,
         firstID:firstID,
@@ -44,6 +47,7 @@ async function countEmojis(guild, channel, mega, uid) {
 
 function megaUpdateDbEmojis (emojiDictID, dbEmojis){
     let uid = emojiDictID.firstID
+    
     for(var key in emojiDictID.emojiDict) {
         client.setEmojis.run({id:key, amount:emojiDictID.emojiDict[key]});
     }
@@ -52,13 +56,16 @@ function megaUpdateDbEmojis (emojiDictID, dbEmojis){
 
 function miniUpdateDbEmojis (emojiDictID, dbEmojis){
     let uid = emojiDictID.firstID
+    
     for(var row in dbEmojis) {
         emojiDictID.emojiDict[dbEmojis[row].id] = (emojiDictID.emojiDict[dbEmojis[row].id] || 0) + dbEmojis[row].amount;
     }
+    
     for(var key in emojiDictID.emojiDict) {
         console.log(key)
         client.setEmojis.run({id:key, amount:emojiDictID.emojiDict[key]});
     }
+    
     client.setUID.run({uid:uid})
 }
 
@@ -66,13 +73,13 @@ client.on("ready", () => {
     console.log("starting up");
     var db = require('./db');
     db.setup(sql);
-
+    
+    // Prepared statements for getting and setting data in DB.
     client.getUID = sql.prepare("SELECT * FROM uidtable");
     client.setUID = sql.prepare("REPLACE INTO uidtable (id, uid) VALUES (0, @uid)");
     
     client.getEmojis = sql.prepare("SELECT * FROM emojis");
     client.setEmojis = sql.prepare("REPLACE INTO emojis (id, amount) VALUES (@id, @amount);");
-    // Prepared statements for getting and setting data in DB.
 });
 
 client.on("message", message => {
@@ -96,19 +103,22 @@ client.on("message", message => {
         if(!message.author.id === message.guild.owner) return message.reply("Only the owner is allowed to mega update the database");
         emojis = client.getEmojis.all();
         emojiArr = message.guild.emojis.array();
-        uid_row = client.getUID.get() || 0
+        uid_row = client.getUID.get() || 0;
+        
         if(cflag === "--mega") {
             countEmojis(message.guild, message.channel, true, uid_row.uid)
                 .then(ceObj => megaUpdateDbEmojis(ceObj, emojis))
                 .then(console.log("success"))
                 .catch(err => console.log(err))
         }
+        
         if(cflag === "--mini") {
             countEmojis(message.guild, message.channel, false, uid_row.uid)
                 .then(ceObj => miniUpdateDbEmojis(ceObj, emojis))
                 .catch(err => console.log(err))
         }
     }
+    
     if(command === "emojilist") {
         console.log("printing emojis")
         var m = ""
